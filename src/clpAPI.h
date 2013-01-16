3b01242b463cde82d0bfba7d0a137ad77f06a1e0
Gabriel Gelius-Dietrich <geliudie@uni-duesseldorf.de>
2013-01-16 00:00:00 +0000

    version 1.2.2

--- src/clpAPI.h
+++ src/clpAPI.h
@@ -1,7 +1,7 @@
 /* clpAPI.h
    R interface to COIN-OR Clp.
  
-   Copyright (C) 2011 Gabriel Gelius-Dietrich, Department for Bioinformatics,
+   Copyright (C) 2011-2013 Gabriel Gelius-Dietrich, Dpt. for Bioinformatics,
    Institute for Informatics, Heinrich-Heine-University, Duesseldorf, Germany.
    All right reserved.
    Email: geliudie@uni-duesseldorf.de
